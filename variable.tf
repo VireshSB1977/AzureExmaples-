81ddc8a5c733469cb64ac06ceb73289381a77133
VireshSB1977 <viresh.sb@zohomail.in>
2026-03-16 22:35:01 +0530

    Initial commit

---
+++ variable.tf
@@ -0,0 +1,25 @@
+variable "resource_group_name" {
+  description = "Azure Resource Group Name"
+  type        = string
+}
+
+variable "location" {
+  description = "Azure Region"
+  type        = string
+}
+
+variable "vm_name" {
+  description = "Virtual Machine Name"
+  type        = string
+}
+
+variable "admin_username" {
+  description = "Admin username for VM"
+  type        = string
+}
+
+variable "admin_password" {
+  description = "Admin password for VM"
+  type        = string
+  sensitive   = true
+}
